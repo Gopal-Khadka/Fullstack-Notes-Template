5b18b3196c19e5860db6e912f6e49725468ae4d8
Gopal Khadka <khadkagopal.mail@gmail.com>
2025-07-04 21:12:31 +0545

    feat(web): update note types and integrate OpenAPI types
    - Refactored note types to utilize OpenAPI-generated types for improved type safety and consistency.
    - Added a new script in package.json to generate TypeScript types from OpenAPI schema.
    - Updated pnpm-lock.yaml to include openapi-typescript as a dependency.
    - Created schema.d.ts file to define API response types for notes.
    - Enhanced note.ts to import types from the generated schema, removing old interface definitions.

--- frontend/apps/web/types/note.ts
+++ frontend/apps/web/types/note.ts
@@ -1,18 +1,28 @@
-export interface Note {
-    id: number;
-    title: string;
-    content: Record<string, any>;
-    created_at: string;
-    updated_at: string;
-  }
-  
-  export interface NoteCreate {
-    title: string;
-    content: Record<string, any>;
-  }
-  
-  export interface NoteUpdate {
-    title: string;
-    content: Record<string, any>;
-  }
-  
+import type { paths, components } from "./schema";
+
+// Schema Obj
+export type NotesListResponse = components["schemas"]["NotesListResponse"];
+export type Note = components["schemas"]["NoteResponse"];
+export type NoteCreate = components["schemas"]["NoteCreate"];
+export type NoteUpdate = components["schemas"]["NoteUpdate"];
+
+export type NoteUpdateError = components["schemas"]["HTTPValidationError"];
+
+// Old Schemas
+// export interface Note {
+//     id: number;
+//     title: string;
+//     content: Record<string, any>;
+//     created_at: string;
+//     updated_at: string;
+//   }
+
+//   export interface NoteCreate {
+//     title: string;
+//     content: Record<string, any>;
+//   }
+
+//   export interface NoteUpdate {
+//     title: string;
+//     content: Record<string, any>;
+//   }
