9448dcf82b734fe16ff40fc5aa903a67e24af9a2
Gopal Khadka <khadkagopal.mail@gmail.com>
2025-07-04 20:53:44 +0545

    feat(web): implement NotesProvider and enhance page functionality
    - Introduced NotesProvider component to manage notes state using React Query.
    - Wrapped the application layout with NotesProvider for context availability.
    - Updated page component to utilize useNotes hook for fetching notes data.
    - Enhanced button functionality to log notes data on click.

--- frontend/apps/web/app/page.tsx
+++ frontend/apps/web/app/page.tsx
@@ -1,11 +1,17 @@
+"use client";
 import { Button } from "@/components/ui/button";
+import { useNotes } from "@/hooks/useNote";
 import React from "react";
 
 const page = () => {
+  const { data, isLoading, error } = useNotes();
+  const handleClick = () => {
+    console.log(data);
+  };
   return (
     <div className="text-quaternary">
       <h1 className="text-primary">Hello World</h1>
-      <Button>Click me</Button>
+      <Button onClick={handleClick}>Click me</Button>
     </div>
   );
 };
